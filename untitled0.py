991fa5485898865b68c26d7544dfdaf35c594524
nattydeacs <natdeacon@gmail.com>
2022-05-26 09:54:18 -0400

    First working filter

--- untitled0.py
+++ untitled0.py
@@ -1,8 +1,56 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Wed May 25 09:52:42 2022
+Created on Thu May 26 09:41:46 2022
 
 @author: natdeacon
 """
 
+import dash
+import dash_html_components as html
+import plotly.graph_objects as go
+import dash_core_components as dcc
+import plotly.express as px
+from dash.dependencies import Input, Output
+
+
+app = dash.Dash()
+
+df = px.data.stocks()
+
+
+app.layout = html.Div(id = 'parent', children = [
+    html.H1(id = 'H1', children = 'Styling using html components', style = {'textAlign':'center',\
+                                            'marginTop':40,'marginBottom':40}),
+
+        dcc.Dropdown( id = 'dropdown',
+        options = [
+            {'label':'Google', 'value':'GOOG' },
+            {'label': 'Apple', 'value':'AAPL'},
+            {'label': 'Amazon', 'value':'AMZN'},
+            ],
+        value = 'GOOG'),
+        dcc.Graph(id = 'bar_plot')
+    ])
+    
+    
+@app.callback(Output(component_id='bar_plot', component_property= 'figure'),
+              [Input(component_id='dropdown', component_property= 'value')])
+def graph_update(dropdown_value):
+    print(dropdown_value)
+    fig = go.Figure([go.Scatter(x = df['date'], y = df['{}'.format(dropdown_value)],\
+                     line = dict(color = 'firebrick', width = 4))
+                     ])
+    
+    fig.update_layout(title = 'Stock prices over time',
+                      xaxis_title = 'Dates',
+                      yaxis_title = 'Prices'
+                      )
+    return fig  
+
+
+
+if __name__ == '__main__': 
+    app.run_server()
+    
+print("test")
